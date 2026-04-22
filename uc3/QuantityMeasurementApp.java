1f25da288dec109391609538d15ec09126573a6e
sambhavi298 <sr3468@srmist.edu.in>
2026-04-22 08:50:44 +0530

    committed uc3

---
+++ uc3/QuantityMeasurementApp.java
@@ -0,0 +1,41 @@
+public class QuantityMeasurementApp {
+
+    enum LengthUnit {
+        FEET(1.0),
+        INCH(1.0 / 12.0);
+
+        private final double factor;
+
+        LengthUnit(double factor) {
+            this.factor = factor;
+        }
+
+        public double toFeet(double value) {
+            return value * factor;
+        }
+    }
+
+    static class QuantityLength {
+        private final double value;
+        private final LengthUnit unit;
+
+        public QuantityLength(double value, LengthUnit unit) {
+            if (unit == null) throw new IllegalArgumentException();
+            this.value = value;
+            this.unit = unit;
+        }
+
+        private double toFeet() {
+            return unit.toFeet(value);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            QuantityLength other = (QuantityLength) obj;
+            return Double.compare(this.toFeet(), other.toFeet()) == 0;
+        }
+    }
+}
